42c7ec4ff0562f66e0103619082d56ca00b6da25
ikalllofc <92963826+ikalllofc@users.noreply.github.com>
2021-10-24 13:48:11 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281220670449', 'KurrTodあ', m)
-  m.reply('wa.me/6281220670449')
+  this.sendContact(m.chat, '6289696454872', 'IKAL BOTZ あ', m)
+  m.reply('wa.me/6289696454872')
 }
 handler.help = ['owner', 'creator', 'pemilikbot']
 handler.tags = ['info']
